e88507649472a19b5f324c14a7fc5038e99b506e
sidazhang <sidazhang89@gmail.com>
2013-05-04 13:22:18 -0700

    downloaded twitter bootstrap

--- public/js/application.js
+++ public/js/application.js
@@ -14,17 +14,17 @@ $(document).ready(function() {
   $('div.survey').on('click',".widget", function(){
     unselectAll('div.survey .widget');
     selectAll(this);
-    hideFirstShowSecond('div.survey .unselected.widget .survey-question-choices','div.survey .selected.widget .survey-question-choices');
+    hideFirstShowSecond('div.survey .unselected.widget #add-choice','div.survey .selected.widget #add-choice');
   });
 
-  $('div.survey').on('click','a#add_question',function(e){
+  $('div.survey').on('click','button#add_question',function(e){
     e.preventDefault();
     unselectAll('div.survey .widget');
-    hideFirstShowSecond('div.survey .unselected.widget .survey-question-choices','div.survey .selected.widget .survey-question-choices');
+    hideFirstShowSecond('div.survey .unselected.widget #add-choice','div.survey .selected.widget #add-choice');
     addItemTo('#survey-inventory > .survey-question-mcq','.widget-container');
   });
 
-  $('div.survey').on('click','a#add-choice',function(e){
+  $('div.survey').on('click','button#add-choice',function(e){
     e.preventDefault();
     addItemTo('#survey-inventory > .survey-question-mcq-q','.widget-container .selected.survey-question-mcq ol');
   });
@@ -40,9 +40,8 @@ $(document).ready(function() {
 
 
 
-
 function findSelectorFindCloestFindChildrenReplaceWith(selector, cloestParent, childSelector,replaceWith){
-      $(selector).closest(cloestParent).children(childSelector).replaceWith($(replaceWith).clone());
+    $(selector).closest(cloestParent).children(childSelector).replaceWith($(replaceWith).clone());
 }
 
 function addItemTo(inventory, appendTo) {
@@ -61,11 +60,14 @@ function hideFirstShowSecond(hideSelector,showSelector){
   showFast(showSelector);
 }
 
+// Example
+
+
 function hideFast(selector){
-  $(selector).hide('fast');
+  $(selector).hide();
 }
 
 
 function showFast(selector){
-  $(selector).show('fast');
+  $(selector).show();
 }
