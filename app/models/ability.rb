867874af80c7945e56a6cae3081d0bb9c2707b93
sidazhang <sidazhang89@gmail.com>
2013-05-05 20:49:38 -0700

    teams merged fully functional

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
 include CanCan::Ability
 
   def initialize(user)
-    user ||= user.new
+    user ||= User.new
     can :read, :all
     can :update, Survey, :user_id => user.id, :live => false
   end
